dcb8d55d09af57358d810b905a7dab5dbb726395
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:08:36 +0200

    Uso de snippets (Operadores relacionales)

---
+++ u2Domingo/00. Snippets/operadoresrelacionales.js
@@ -0,0 +1,16 @@
+var numero1 = 3;
+var numero2 = 5;
+resultado = numero1 > numero2; // resultado = false
+console.log(resultado);
+resultado = numero1 < numero2; // resultado = true
+console.log(resultado);
+numero1 = 5;
+numero2 = 5;
+resultado = numero1 >= numero2; // resultado = true
+console.log(resultado);
+resultado = numero1 <= numero2; // resultado = true
+console.log(resultado);
+resultado = numero1 == numero2; // resultado = true
+console.log(resultado);
+resultado = numero1 != numero2; // resultado = false
+console.log(resultado);
