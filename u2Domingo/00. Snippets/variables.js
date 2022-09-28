262e220dac1f0bb67614c782b4694cc36a7837dd
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:06:37 +0200

    Uso de snippets (Variables)

---
+++ u2Domingo/00. Snippets/variables.js
@@ -0,0 +1,35 @@
+/*var coche;
+var coche;
+var contador;
+4+2;
+
+casa;
+*/
+
+/*
+var numero_1;
+var numero_2;
+
+numero_1 = 3;
+numero_2 = 1;
+var resultado = numero_1 + numero_2;
+
+console.log( 'Resultado: ' + resultado );
+
+*/
+
+/*
+var $cosa;
+// var #contador;
+
+*/
+
+var iva = 21;
+var subtotal = 123.23;
+
+console.log('IVA: ' + iva);
+console.log('Subtotal: ' + subtotal);
+
+iva = subtotal;
+
+console.log('IVA: ' + iva);
