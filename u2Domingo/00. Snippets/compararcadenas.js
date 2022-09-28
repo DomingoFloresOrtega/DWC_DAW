156caff0fc2b32c0b8ee0ae6abf48a8ced58211f
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:17:22 +0200

    Uso de snippets (Comparar cadenas)

---
+++ u2Domingo/00. Snippets/compararcadenas.js
@@ -0,0 +1,13 @@
+var a = 'a';
+var A = 'A';
+var comparaLetras = a > A;
+
+comparaLetras;
+
+a.charCodeAt(0);
+A.charCodeAt(0);
+
+console.log('hola'>'Hola');
+console.log('hola'<'Hola');
+console.log('hola'<'holaa');
+console.log('hola'>'holaa');
