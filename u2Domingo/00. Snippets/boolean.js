a8987ca63c601004ae543e9ec8679c42f7599d9b
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:06:54 +0200

    Uso de snippets (Boolean)

---
+++ u2Domingo/00. Snippets/boolean.js
@@ -0,0 +1,11 @@
+var estaLloviendo = false;
+
+// estaLloviendo = 'verdadero';
+
+estaLloviendo;
+
+var recipienteRelleno = Boolean(true);
+
+var mensaje = 'hola';
+
+var mensaje2 = String('hola mundo');
