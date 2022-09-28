f46959b16e2ff16178f7bcbc7d44485fa9c7b36e
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:07:59 +0200

    Uso de snippets (Operadores matemáticos)

---
+++ u2Domingo/00. Snippets/operadoresmatematicos.js
@@ -0,0 +1,12 @@
+var numero1 = 5;
+numero1 += 3; // numero1 = numero1 + 3 = 8
+console.log(numero1);
+numero1 -= 1; // numero1 = numero1 - 1 = 4
+console.log(numero1);
+numero1 *= 2; // numero1 = numero1 * 2 = 10
+console.log(numero1);
+numero1 /= 5; // numero1 = numero1 / 5 = 1
+console.log(numero1);
+numero1 %= 4; // numero1 = numero1 % 4 = 1
+console.log(numero1);
+
