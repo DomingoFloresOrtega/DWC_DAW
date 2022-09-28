caca6694257d44d16510b38cd9745594ca4251f0
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:07:49 +0200

    Uso de snippets (Operadores lógicos)

---
+++ u2Domingo/00. Snippets/operadoreslogicos.js
@@ -0,0 +1,19 @@
+/*
+var relleno = 0;
+var terminado = true;
+
+if ( Boolean(3 > 2) ){
+    relleno = 40;
+}
+
+
+if ((relleno == true) && terminado){
+    console.log('Fin de proceso');
+}
+
+if (relleno && !terminado){
+    console.log('En proceso');
+}
+
+console.log('prueba');
+*/
