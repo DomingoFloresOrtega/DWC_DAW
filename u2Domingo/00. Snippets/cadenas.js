5c0adbf74b709889b2083b9949336e23e41955c2
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:07:03 +0200

    Uso de snippets (Cadenas)

---
+++ u2Domingo/00. Snippets/cadenas.js
@@ -0,0 +1,11 @@
+/*
+var mensaje = "Bienvenido a nuestro sitio web";
+var nombreProducto = 'Producto ABC';
+var letraSeleccionada = 'c';
+*/
+
+var texto = "Hola Mundo!\n";
+var texto2 = "\tQué facil es incluir \'comillas simples\'\n";
+var texto3 = "\ty \"comillas dobles\"";
+
+alert(texto+texto2+texto3);
