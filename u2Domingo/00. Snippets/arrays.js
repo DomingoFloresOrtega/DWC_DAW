dd4a7960217fdf1afe8c10a560de5c7654def207
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-28 10:06:45 +0200

    Uso de snippets (Arrays)

---
+++ u2Domingo/00. Snippets/arrays.js
@@ -0,0 +1,16 @@
+/*
+var dias = [];
+
+dias[0] = 'lunes';
+dias[1] = 'martes';
+
+console.log('dias: ' + dias);
+
+var valores = [ 'lunes', true, 20.45, 34, "medición"];
+
+console.log('valores: ' + valores);
+*/
+
+var meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'];
+
+alert(meses);
