78eeca9084fcb9b1718f88262adc1562065a31ae
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-09-30 13:41:04 +0200

    Uso de snippets (if else if)

---
+++ u2Domingo/00. Snippets/ifelseif.js
@@ -0,0 +1,14 @@
+edad = 34;
+
+if (edad < 12) {
+    alert('Todavía eres muy pequeño');
+}
+else if (edad < 19){
+    alert('Eres un adolescente');
+}
+else if (edad < 35){
+    alert('Aun sigues siendo joven');
+}
+else {
+    alert('Piensa en cuidarte un poco más');
+}
