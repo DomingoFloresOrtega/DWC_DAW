e3a67d5361f0eb08716ac6b1e9aa7d40feaa31eb
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2022-10-17 08:36:50 +0200

    Uso de snippets (Math)

---
+++ u3Domingo/00. Snippets/math.js
@@ -0,0 +1,10 @@
+var r = prompt("Ingresa el radio de un c\xedrculo:");
+var area = Math.PI * Math.pow(r, 2);
+alert("El \xe1rea del c\xedrculo es de: " + area + " cms cuadrados");
+
+var b = prompt("Ingresa la base:");
+var a = prompt("Ingresa la altura:");
+
+var area2 = Math.pow(b*a, 2);
+
+alert("El resultado es: " + area2);
