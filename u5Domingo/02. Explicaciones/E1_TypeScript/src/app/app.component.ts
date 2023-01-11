04085d69ad07d380e3e0d730df100659e1cf60c6
Domingo Flores Ortega <domingo@iesnuevaandalucia.com>
2023-01-11 10:12:02 +0100

    Explicaciones de TypeScript (Interfaces y Constantes)

--- u5Domingo/02. Explicaciones/E1_TypeScript/src/app/app.component.ts
+++ u5Domingo/02. Explicaciones/E1_TypeScript/src/app/app.component.ts
@@ -34,9 +34,69 @@ console.log('Viaje', unViaje);
 
 
 // Funciones operaciones
-function suma(a: number, b: number):number {
-  return (a + b);
+function suma(
+    a: number,
+    b?: number, // ? - opcional
+    c: number = 3) {
+        return (a + c);
 }
 
-let res = suma(3, 45);
+let res = suma(3);
 console.log(res);
+
+// Ejemplo 1
+
+interface IntObj {
+  a: string,
+  b: string,
+  c: number;
+}
+
+const nombreObj: IntObj = {
+  a: 'primer valor',
+  b: 'segundo valor',
+  c: 45
+};
+
+function mostrarObjeto (o: IntObj) {
+  console.log(o);
+  o.b
+};
+
+mostrarObjeto(nombreObj);
+
+// Ejercicio 1
+
+interface ElViaje {
+  nombre: string,
+  origen: string,
+  destino: string,
+  ruta: TipoRuta,
+  mostrarDestino: () => string,
+  mostrarTransporte: () => string
+}
+
+interface TipoRuta {
+  transporte: string,
+  paradas: string[];
+}
+
+const elviaje = {
+  nombre: 'Viaje fin de curso',
+  origen: 'Malaga',
+  destino: 'Marrakesh',
+  ruta: {
+      transporte: 'Coche',
+      paradas: ['Algeciras', 'Tánger', 'Casablanca']
+  },
+  mostrarDestino() {
+    return this.destino;
+  },
+  mostrarTransporte() {
+    return this.ruta.transporte;
+  }
+};
+
+const dest = elviaje.mostrarDestino();
+const trans = elviaje.mostrarTransporte();
+console.log(dest,trans);
